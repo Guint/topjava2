e980e5c656b11993f3a7fc19720ed940a44b57b1
Grigory Kislin <javawebinar@yandex.ru>
2018-04-26 05:56:43 +0300

    8_07_ajax_datatables

---
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminAjaxController.java
@@ -0,0 +1,37 @@
+package ru.javawebinar.topjava.web.user;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ajax/admin/users")
+public class AdminAjaxController extends AbstractUserController {
+
+    @Override
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<User> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") int id) {
+        super.delete(id);
+    }
+
+    @PostMapping
+    public void createOrUpdate(@RequestParam("id") Integer id,
+                               @RequestParam("name") String name,
+                               @RequestParam("email") String email,
+                               @RequestParam("password") String password) {
+
+        User user = new User(id, name, email, password, Role.ROLE_USER);
+        if (user.isNew()) {
+            super.create(user);
+        }
+    }
+}
