1a1dab009aff06e77ba52b0686df6e13dbd5f028
Grigory Kislin <javawebinar@yandex.ru>
2018-04-26 10:29:36 +0300

    8_08_notification

--- src/main/webapp/resources/js/datatablesUtil.js
+++ src/main/webapp/resources/js/datatablesUtil.js
@@ -2,6 +2,13 @@ function makeEditable() {
     $(".delete").click(function () {
         deleteRow($(this).attr("id"));
     });
+
+    $(document).ajaxError(function (event, jqXHR, options, jsExc) {
+        failNoty(jqXHR);
+    });
+
+    // solve problem with cache in IE: https://stackoverflow.com/a/4303862/548473
+    $.ajaxSetup({cache: false});
 }
 
 function add() {
@@ -15,6 +22,7 @@ function deleteRow(id) {
         type: "DELETE",
         success: function () {
             updateTable();
+            successNoty("Deleted");
         }
     });
 }
@@ -34,6 +42,35 @@ function save() {
         success: function () {
             $("#editRow").modal("hide");
             updateTable();
+            successNoty("Saved");
         }
     });
+}
+
+var failedNote;
+
+function closeNoty() {
+    if (failedNote) {
+        failedNote.close();
+        failedNote = undefined;
+    }
+}
+
+function successNoty(text) {
+    closeNoty();
+    new Noty({
+        text: "<span class='fa fa-lg fa-check'></span> &nbsp;" + text,
+        type: 'success',
+        layout: "bottomRight",
+        timeout: 1000
+    }).show();
+}
+
+function failNoty(jqXHR) {
+    closeNoty();
+    failedNote = new Noty({
+        text: "<span class='fa fa-lg fa-exclamation-circle'></span> &nbsp;Error status: " + jqXHR.status,
+        type: "error",
+        layout: "bottomRight"
+    }).show();
 }
