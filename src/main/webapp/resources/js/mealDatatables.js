a7929195e64686ab02ed527d96c67a41d358eb82
Grigory Kislin <javawebinar@yandex.ru>
2018-05-08 18:05:47 +0300

    10_04_jquery_converters

--- src/main/webapp/resources/js/mealDatatables.js
+++ src/main/webapp/resources/js/mealDatatables.js
@@ -14,6 +14,19 @@ function clearFilter() {
     $.get(ajaxUrl, updateTableByData);
 }
 
+// http://api.jquery.com/jQuery.ajax/#using-converters
+$.ajaxSetup({
+    converters: {
+        "text json": function (stringData) {
+            var json = JSON.parse(stringData);
+            $(json).each(function () {
+                this.dateTime = this.dateTime.replace('T', ' ').substr(0, 16);
+            });
+            return json;
+        }
+    }
+});
+
 $(function () {
     datatableApi = $("#datatable").DataTable({
         "ajax": {
@@ -24,13 +37,7 @@ $(function () {
         "info": true,
         "columns": [
             {
-                "data": "dateTime",
-                "render": function (date, type, row) {
-                    if (type === 'display') {
-                        return formatDate(date);
-                    }
-                    return date;
-                }
+                "data": "dateTime"
             },
             {
                 "data": "description"
