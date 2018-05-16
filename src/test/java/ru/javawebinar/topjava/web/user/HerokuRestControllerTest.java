31afeea9d30f3c424ec71f253c45240a34ffc6e2
Grigory Kislin <javawebinar@yandex.ru>
2018-05-17 02:57:17 +0300

    11_11_heroku

--- src/test/java/ru/javawebinar/topjava/web/user/HerokuRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/HerokuRestControllerTest.java
@@ -1,11 +1,17 @@
 package ru.javawebinar.topjava.web.user;
 
 import org.junit.Test;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePropertySource;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.util.exception.ErrorType;
 import ru.javawebinar.topjava.web.AbstractControllerTest;
+
+import java.io.IOException;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -20,6 +26,23 @@ import static ru.javawebinar.topjava.util.exception.ModificationRestrictionExcep
 public class HerokuRestControllerTest extends AbstractControllerTest {
 
     private static final String REST_URL = AdminRestController.REST_URL + '/';
+
+    // Set DATABASE_URL environment for heroku profile
+    static {
+        Resource resource = new ClassPathResource("db/postgres.properties");
+        try {
+            PropertySource propertySource = new ResourcePropertySource(resource);
+            String herokuDbUrl = String.format("postgres://%s:%s@%s",
+                    propertySource.getProperty("database.username"),
+                    propertySource.getProperty("database.password"),
+                    ((String) propertySource.getProperty("database.url")).substring(18));
+            System.out.println(herokuDbUrl);
+
+            System.setProperty("DATABASE_URL", herokuDbUrl);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 
     @Test
     public void testDelete() throws Exception {
